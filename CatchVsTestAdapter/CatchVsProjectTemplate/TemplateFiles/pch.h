23ff9b3475044da47fab07a3ad68fbfbff3c848a
Sean D. Cline <sean.d.cline@gmail.com>
2014-10-05 13:29:30 -0400

    Integrate @bkuhns' project template into the extension.

---
+++ CatchVsTestAdapter/CatchVsProjectTemplate/TemplateFiles/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "catch.hpp"
+
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <memory>
