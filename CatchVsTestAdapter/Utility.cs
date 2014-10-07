8d3edb253b3c9ccf815d1f11a8b46ffefc05ae0b
Sean D. Cline <sean.d.cline@gmail.com>
2014-10-07 18:45:10 -0400

    Replace some ugly procedural code with linq.

--- CatchVsTestAdapter/Utility.cs
+++ CatchVsTestAdapter/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace CatchVsTestAdapter
@@ -8,8 +9,8 @@ namespace CatchVsTestAdapter
     {
         public static string runExe(string exe, params string[] args)
         {
-            var p = new Process{};
-            p.StartInfo = new ProcessStartInfo{};
+            var p = new Process { };
+            p.StartInfo = new ProcessStartInfo { };
             p.StartInfo.CreateNoWindow = true;
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
@@ -24,19 +25,7 @@ namespace CatchVsTestAdapter
 
         public static String escapeArguments(params string[] args)
         {
-            var argumentString = "";
-            foreach (var arg in args)
-            {
-                argumentString += escapeArgument(arg) + " ";
-            }
-
-            // Trim the trailing space from above.
-            if (argumentString.Length > 0)
-            {
-                argumentString = argumentString.Remove(argumentString.Length - 1);
-            }
-
-            return argumentString;
+            return args.Select(escapeArgument).Aggregate((x, y) => x + " " + y);
         }
 
         internal static string escapeArgument(string arg)
